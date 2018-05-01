1b21771f50a500a4a46652718df5966f5961daee
mukul96 <mukulkathpalia@gmail.com>
2018-05-01 19:56:55 +0530

    static urls added

--- Intern_work/settings.py
+++ Intern_work/settings.py
@@ -118,7 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
